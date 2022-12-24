ff653559e82bc33a186b4687fb4381d8571baf1c
Duviel Garcia <duviel7@gmail.com>
2022-12-23 20:38:07 -0500

    chore: add enviroments values

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
+process.env.APP_ENV = process.env.NODE_ENV ?? 'development';
+
+dotenv.config({
+  path: path.join(__dirname, `/../config/${process.env.APP_ENV}.env`),
+});
+
+console.log(process.env.APP_FOO);
 
 const app: express.Application = express();
 
