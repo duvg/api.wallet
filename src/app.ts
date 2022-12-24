3d09f7256b6ce3bf6d24610c7286599180d60572
Duviel Garcia <duviel7@gmail.com>
2022-12-23 20:53:15 -0500

    chore: add awilix package and config IOC container

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import { container } from './container';
+import { TestService } from './services/test.service';
 
 process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
 process.env.APP_ENV = process.env.NODE_ENV ?? 'development';
@@ -16,5 +18,9 @@ const app: express.Application = express();
 app.get('/', (req, res) => {
   res.send('Hello duvg...');
 });
+
+const testService = container.resolve<TestService>('testService');
+
+console.log(testService.get());
 
 export { app };
